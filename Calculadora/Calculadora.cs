e09b5b1a6e1782c00ebfd7b35ae32e412a7a1edb
SanchezMariaVictoria2005 <mvictoria14092005@gmail.com>
2026-06-01 20:52:10 -0300

    definiendo clase calculadora

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,12 @@
+namespace EspacioCalculadora
+{
+    public class Calculadora
+    {
+        private double dato;
+
+        public void Sumar (double termino)
+        {
+            
+        }
+    }
+}
