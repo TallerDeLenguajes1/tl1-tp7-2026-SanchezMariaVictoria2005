645f8ebbae9fe43d16a07732bf7b900289e85e74
SanchezMariaVictoria2005 <mvictoria14092005@gmail.com>
2026-06-02 01:01:43 -0300

    prueba metodo jubilacion

--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -3,9 +3,10 @@ DateTime fechaNacimiento = new DateTime(1994,5,2);
 DateTime fechaIngreso = new DateTime(2015,7,15);
 
 Empleado empleado1 = new Empleado("yo", "diaz", fechaNacimiento, 'c', fechaIngreso, 1000, 2);
-empleado1.Antiguedad();
+
+
 Console.WriteLine(empleado1.FechaIngreso);
 Console.WriteLine(empleado1.FechaNacimiento);
 empleado1.Edad();
-
+empleado1.jubilacion();
 Console.WriteLine("fin del programa");
