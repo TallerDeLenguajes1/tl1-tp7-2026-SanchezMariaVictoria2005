23074190a490137ea62f9f1a385ce5c362a29ff0
SanchezMariaVictoria2005 <mvictoria14092005@gmail.com>
2026-06-01 21:51:26 -0300

    clase empleado definida

---
+++ SisPersonal/Empleado.cs
@@ -0,0 +1,15 @@
+namespace Empleados
+{
+    public class Empleados
+    {
+        private string nombre;
+        private string apellido;
+        private DateTime fechaNacimiento;
+        private char estadoCivil;
+        private DateTime fechaIngreso;
+        private double sueldoBasico;
+
+        //private cargos Cargo
+
+    }
+}
