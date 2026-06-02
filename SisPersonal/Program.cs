c7c5494a20a80caa129b76f51b5cd1f5e1d36b33
SanchezMariaVictoria2005 <mvictoria14092005@gmail.com>
2026-06-02 00:05:01 -0300

    metodo para calcular la antiguedad de un empleado funcionando

--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -3,5 +3,6 @@ DateTime fechaNacimiento = new DateTime(1994,5,2);
 DateTime fechaIngreso = new DateTime(2015,7,15);
 
 Empleado empleado1 = new Empleado("yo", "diaz", fechaNacimiento, 'c', fechaIngreso, 1000, 2);
+empleado1.Antiguedad();
 
 Console.WriteLine("fin del programa");
