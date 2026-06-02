26b55099d4d69214923f455b1dd20bce2415eeb0
SanchezMariaVictoria2005 <mvictoria14092005@gmail.com>
2026-06-01 22:07:18 -0300

    constructor de la clase empleado definido

--- SisPersonal/Empleado.cs
+++ SisPersonal/Empleado.cs
@@ -1,6 +1,6 @@
 namespace Empleados
 {
-    public class Empleados
+    public class Empleado
     {
         private string nombre;
         private string apellido;
@@ -11,6 +11,17 @@ namespace Empleados
 
         private Cargos cargo;
 
+        public Empleado(string nombre, string apellido, DateTime fechaNacimiento, char estadoCivil, DateTime fechaIngreso, double sueldoBasico, int cargo)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.fechaNacimiento = fechaNacimiento;
+            this.estadoCivil = estadoCivil;
+            this.fechaIngreso = fechaIngreso;
+            this.sueldoBasico = sueldoBasico;
+            this.cargo = (Cargos)cargo;
+        }
+
     }
 
     enum Cargos
@@ -20,5 +31,8 @@ namespace Empleados
        Ingeniero,
        Especialista,
        Investigado 
+
     }
+
+
 }
